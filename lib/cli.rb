b81422ca2c5782ce1a480b1b2202377462b48cc9
steveafrost <ffrostt@gmail.com>
2016-06-16 18:50:13 -0500

    stub complete for retrieving scores

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,7 +3,30 @@
 class Scoreboard::CLI
   
   def start
-    puts "Welcome to Scoreboard! Please type 'scores' to see yesterday's MLB scores."
+    puts "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
+    puts "*~* Welcome to Scoreboard! Please type 'scores' to see yesterday's MLB scores *~*"
+    puts "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*"
+    puts "\n"
+    user_input = nil
+    user_input = gets.strip
+    case user_input
+    when "scores"
+      score_recap
+    when "exit"
+      exit
+    else
+      start
+    end
+  end
+  
+  def score_recap
+    puts "\n"
+    puts "Detroit Tigers 1"
+    puts "Kansas City Royals 0"
+    puts "------------------------"
+    puts "Chicago Cubs 15"
+    puts "Washington Nationals 9"
+    puts "------------------------"
   end
   
 end
